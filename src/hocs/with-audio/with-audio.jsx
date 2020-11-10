9b6a19eb0b2e439ff560df2eb0179851dbdfeb7e
knockyLoNe <knocky4u@gmail.com>
2020-11-11 00:59:49 +0600

    module6-task1

---
+++ src/hocs/with-audio/with-audio.jsx
@@ -0,0 +1,32 @@
+import React, {Fragment} from "react";
+import PropTypes from "prop-types";
+
+const AudioPlayer = (props) => {
+  const {isLoading, onPlayButtonClick, isPlaying, children} = props;
+
+  return (
+    <Fragment>
+      <button
+        className={`track__button track__button--${isPlaying ? `pause` : `play`}`}
+        type="button"
+        disabled={isLoading}
+        onClick={onPlayButtonClick}
+      />
+      <div className="track__status">
+        {children}
+      </div>
+    </Fragment>
+  );
+};
+
+AudioPlayer.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  isPlaying: PropTypes.bool.isRequired,
+  onPlayButtonClick: PropTypes.func.isRequired,
+};
+
+export default AudioPlayer;
